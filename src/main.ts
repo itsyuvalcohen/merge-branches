3939eb52f3472d63c3546e223b3623138de31af3
Yuval Cohen <yuvalco100@gmail.com>
2023-07-11 14:45:23 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {GitHub} from '@actions/github/lib/utils'
 import {WebhookPayload} from '@actions/github/lib/interfaces'
+import {RequestError} from '@octokit/types'
 
 async function run(): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   try {
     const targetBranch: string = core.getInput('target_branch', {required: true})
     const commitMessage: string = core.getInput('message', {required: true})
@@ -20,11 +23,8 @@ async function run(): Promise<void> {
 
     await mergeBranch(octokit, targetBranch, branchName, commitMessage)
 
-    core.setOutput(
-      'message',
-      `Merged branch ${branchName} into ${targetBranch}`
-    )
-  } catch (error: any) {
+    core.info(`Merged branch ${branchName} into ${targetBranch}`)
+  } catch (error: RequestError) {
     core.setFailed(error.message)
   }
 }
@@ -35,20 +35,36 @@ async function mergeBranch(
   branchName: string,
   commitMessage: string
 ): Promise<void> {
-  await octokit.rest.git.createRef({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    ref: `refs/heads/${baseBranch}`,
-    sha: github.context.sha
-  })
+  const owner: string = github.context.repo.owner
+  const repo: string = github.context.repo.repo
 
-  await octokit.rest.repos.merge({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    base: baseBranch,
-    head: branchName,
-    commit_message: commitMessage
-  })
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  try {
+    // Attempt to perform the merge operation
+    await octokit.rest.repos.merge({
+      owner,
+      repo,
+      base: baseBranch,
+      head: branchName,
+      commit_message: commitMessage
+    })
+  } catch (error: RequestError) {
+    // If a 409 conflict error occurs, create a pull request instead
+    if (error.status === 409) {
+      const pullRequest = await octokit.rest.pulls.create({
+        owner,
+        repo,
+        title: `Merge ${branchName} into ${baseBranch}`,
+        head: branchName,
+        base: baseBranch,
+        body: 'Automatic merge conflict, please resolve manually.'
+      })
+      core.info(`Pull request created: ${pullRequest.data.html_url}`)
+    } else {
+      throw error
+    }
+  }
 }
 
 run()
