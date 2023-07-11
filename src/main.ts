546d661c66cb72183542681b632d2b49d0bb7ef0
Yuval Cohen <85868677+itsyuvalcohen@users.noreply.github.com>
2023-07-11 13:53:18 +0300

    init (#7)

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,54 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {GitHub} from '@actions/github/lib/utils'
+import {WebhookPayload} from '@actions/github/lib/interfaces'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const targetBranch: string = core.getInput('target_branch', {required: true})
+    const commitMessage: string = core.getInput('message', {required: true})
+    const githubToken: string = core.getInput('github_token', {required: true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit: InstanceType<typeof GitHub> = github.getOctokit(githubToken)
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const payload: WebhookPayload = github.context.payload
+    if (!payload || !payload.ref) {
+      throw new Error('Invalid payload. Could not find the branch information.')
+    }
+
+    const branchName = payload.ref.replace('refs/heads/', '')
+
+    await mergeBranch(octokit, targetBranch, branchName, commitMessage)
+
+    core.setOutput(
+      'message',
+      `Merged branch ${branchName} into ${targetBranch}`
+    )
+  } catch (error: any) {
+    core.setFailed(error.message)
   }
+}
+
+async function mergeBranch(
+  octokit: InstanceType<typeof GitHub>,
+  baseBranch: string,
+  branchName: string,
+  commitMessage: string
+): Promise<void> {
+  await octokit.rest.git.createRef({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    ref: `refs/heads/${baseBranch}`,
+    sha: github.context.sha
+  })
+
+  await octokit.rest.repos.merge({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    base: baseBranch,
+    head: branchName,
+    commit_message: commitMessage
+  })
 }
 
 run()
